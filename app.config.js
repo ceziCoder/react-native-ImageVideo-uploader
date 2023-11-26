dd6f50664b93c77da0a3bc727daa5ad1fb851c70
cezicoder <cezarylachut1981@gmail.com>
2023-11-26 16:24:43 +0100

    m

--- app.config.js
+++ app.config.js
@@ -1,7 +1,11 @@
 import 'dotenv/config';
 
 export default {
-
+  "runtimeVersion": "1.0.0",
+  "updates": {
+    "url": "https://u.expo.dev/a4e12a8c-a44c-4146-91c0-895277b2ef2a"
+  },
+  "platforms": ["ios", "android", "web"],
    extra: {
 
         apiKey: process.env.API_KEY,
@@ -10,9 +14,9 @@ export default {
         storageBucket: process.env.STORAGE_BUCKET,
         messagingSenderId: process.env.MESSAGING_SENDER_ID,
     appId: process.env.APP_ID,
-    eas: {
-      projectId: process.env.EAS_PROJECT_ID,
-    },
+    "eas": {
+      "projectId": "a4e12a8c-a44c-4146-91c0-895277b2ef2a"
+    }
    
   
       }
