a5aeb134bd6c77568bbc44122be1d88da359661a
cezicoder <cezarylachut1981@gmail.com>
2023-11-26 14:26:48 +0100

    publishing

--- app.config.js
+++ app.config.js
@@ -1,7 +1,11 @@
 import 'dotenv/config';
 
 export default {
-
+  owner: 'cezicoder',
+  "updates": {
+    "url": "https://u.expo.dev/a4e12a8c-a44c-4146-91c0-895277b2ef2a"
+  },
+  "runtimeVersion": "1.0.0",
     extra: {
 
         apiKey: process.env.API_KEY,
@@ -9,7 +13,11 @@ export default {
         projectId: process.env.PROJECT_ID,
         storageBucket: process.env.STORAGE_BUCKET,
         messagingSenderId: process.env.MESSAGING_SENDER_ID,
-        appId: process.env.APP_ID
+    appId: process.env.APP_ID,
+    eas: {
+      projectId: process.env.EAS_PROJECT_ID,
+    },
+   
   
       }
 
